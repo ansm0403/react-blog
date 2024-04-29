5cc28abd04a463238023d9316dd0af899260f177
an-sangmoon <tkdans312@gmail.com>
2024-04-30 01:53:11 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          크리스티아노
         </p>
         <a
           className="App-link"
