09f1c80a0ddb83b29d374082534992d91f500e18
an-sangmoon <tkdans312@gmail.com>
2024-04-30 03:00:53 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, Navigate, Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          크리스티아노
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Link to ='/'>Home</Link>
+        <Link to ='/posts'>post</Link>
+      </div>
+      <Routes>
+        <Route path = '/' element={<h1>Home</h1>} />
+        <Route path = '/posts' element={<h1>post</h1>} />
+        <Route path = '/posts/:id' element={<h1>post</h1>} />
+        <Route path = '/posts/new' element={<h1>post</h1>} />
+        <Route path = '/posts/edit/:id' element={<h1>post</h1>} />
+        <Route path = '/profile' element={<h1>post</h1>} />
+        <Route path = '*' element={<Navigate replace to = "/" />} />
+      </Routes>
+    </>
   );
 }
 
